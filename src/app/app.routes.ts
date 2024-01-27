e676c14a235c77b79e3012917a507d218560a831
Justas <justaslescinskas@gmail.com>
2024-01-27 18:38:19 +0200

    Error messages for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'home', component: HomePageComponent },
 ];
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,19 +1,70 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
-  password?: string;
   showPassword: boolean = false;
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
+  }
+
+  //TODO: Add authentication service
+  //constructor(private authService: AuthService) {}
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]], // Validate email as required and with email format
+      password: ['', [Validators.required]], // Validate password as required
+    });
+  }
+
+  onLoginClick() {
+    // Check if the form is valid
+    const formData = this.loginForm.value;
+    console.log(this.loginForm.get('email'));
+    console.log(this.loginForm.get('email')?.hasError('required'));
+    if (this.loginForm.invalid) {
+      // The form is invalid, do not proceed with login
+      console.log('Form is invalid');
+      console.log('Email:', formData.email);
+      console.log('Password:', formData.password);
+      return;
+    }
+    // Gather form data
+    // Form is valid, proceed with login
+    //const formData = this.loginForm.value;
+    console.log('Email:', formData.email);
+    console.log('Password:', formData.password);
+
+    // Send the data to the backend
+    /*this.authService.login(formData).subscribe((response) => {
+      if (response === true) {
+        // Redirect to the home page if the response is true
+        this.router.navigate(['/home']); // Import and inject Router
+      } else {
+        // Handle login failure (e.g., show error message)
+      }
+    });*/
+
+    this.router.navigate(['/home']);
   }
 }
